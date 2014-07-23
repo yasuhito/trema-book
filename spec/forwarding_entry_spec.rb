684af52bfd4a03c4f5dd76b57e3e3c94e303a533
Yasuhito Takamiya <yasuhito@gmail.com>
2014-07-23 09:30:52 +0900

    Add spec/forwarding_entry_spec.rb.

---
+++ spec/forwarding_entry_spec.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+
+require 'forwarding_entry'
+
+describe ForwardingEntry do
+  describe '.new' do
+    context 'with port_no = 1' do
+      Given(:entry) { ForwardingEntry.new('11:22:33:44:55:66', 1, 1, 0x123) }
+
+      describe '#update' do
+        context 'with 2' do
+          When { entry.update(2) }
+          Then { entry.port_no == 2 }
+        end
+
+        context 'with 1' do
+          When { entry.update(1) }
+          Then { entry.port_no == 1 }
+        end
+      end
+    end
+
+    context 'with age_max = 0' do
+      Given(:entry) { ForwardingEntry.new('11:22:33:44:55:66', 1, 0, 0x123) }
+      Then { entry.aged_out? == true }
+    end
+
+    context 'when age_max = 10' do
+      Given(:entry) { ForwardingEntry.new('11:22:33:44:55:66', 1, 10, 0x123) }
+      Then { entry.aged_out? == false }
+    end
+  end
+end
