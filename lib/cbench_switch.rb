c889195e25e95451ba7031fd7bb3b398739e5e9f
Yasuhito Takamiya <yasuhito@gmail.com>
2015-02-06 15:17:45 +0900

    Automatically respond to Echo request messages from switches.

--- lib/cbench_switch.rb
+++ lib/cbench_switch.rb
@@ -3,7 +3,6 @@ class CbenchSwitch < Trema::Controller
   def packet_in(datapath_id, message)
     send_flow_mod_add(
       datapath_id,
-      cookie: 0,
       match: ExactMatch.new(message),
       buffer_id: message.buffer_id,
       actions: SendOutPort.new(message.in_port + 1)
