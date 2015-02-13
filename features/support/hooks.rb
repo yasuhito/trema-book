63662bec10bac0e2ba43d8e0265bcc378c26f4af
Yasuhito Takamiya <yasuhito@gmail.com>
2015-02-13 18:23:25 +0900

    Increase timeout secounds.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,7 +1,7 @@
 Before('@sudo') do
   fail 'sudo authentication failed' unless system 'sudo -v'
-  @aruba_timeout_seconds = 20
-  @aruba_io_wait_seconds = 20
+  @aruba_timeout_seconds = 30
+  @aruba_io_wait_seconds = 30
 end
 
 After('@sudo') do
