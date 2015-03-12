553b8961258f93c72ce265c59ffd4d53cba7d5bd
Yasuhito Takamiya <yasuhito@gmail.com>
2015-03-12 10:55:15 +0900

    Refactoring.

--- lib/topology.rb
+++ lib/topology.rb
@@ -1,20 +1,32 @@
 $LOAD_PATH.unshift __dir__
 
 require 'link'
-require 'observer'
 
 # Topology information containing the list of known switches, ports,
 # and links.
 class Topology
-  include Observable
+  Port = Struct.new(:dpid, :number) do
+    def <=>(other)
+      number <=> other.number
+    end
+
+    def to_s
+      "Switch #{format '%#x', dpid} (port=#{number})"
+    end
+  end
 
   attr_reader :links
   attr_reader :ports
 
   def initialize
+    @observers = []
     @ports = Hash.new { [].freeze }
     @links = []
     @hosts = []
+  end
+
+  def add_observer(observer)
+    @observers << observer
   end
 
   def switches
@@ -23,42 +35,39 @@ class Topology
 
   def add_switch(dpid, ports)
     ports.each { |each| add_port(each) }
-    changed
-    notify_observers :add_switch, dpid, self
+    maybe_callback :add_switch, dpid, self
   end
 
   def delete_switch(dpid)
     delete_port(@ports[dpid].pop) until @ports[dpid].empty?
     @ports.delete dpid
-    changed
-    notify_observers :delete_switch, dpid, self
+    maybe_callback :delete_switch, dpid, self
   end
 
   def add_port(port)
     @ports[port.dpid] += [port]
-    changed
-    notify_observers :add_port, port, self
+    maybe_callback :add_port, Port.new(port.dpid, port.number), self
   end
 
   def delete_port(port)
     @ports[port.dpid].delete_if { |each| each.number == port.number }
-    changed
-    notify_observers :delete_port, port, self
+    maybe_callback :delete_port, Port.new(port.dpid, port.number), self
     maybe_delete_link port
   end
 
   def maybe_add_link(link)
     return if @links.include?(link)
     @links << link
-    changed
-    notify_observers :add_link, link, self
+    port_a = Port.new(link.dpid_a, link.port_a)
+    port_b = Port.new(link.dpid_b, link.port_b)
+    maybe_callback :add_link, port_a, port_b, self
   end
 
   def maybe_add_host(*host)
     return if @hosts.include?(host)
     @hosts << host
-    changed
-    notify_observers :add_host, host, self
+    mac_address, _ip_address, dpid, port_no = *host
+    maybe_callback :add_host, mac_address, Port.new(dpid, port_no), self
   end
 
   def route(ip_source_address, ip_destination_address)
@@ -70,9 +79,17 @@ class Topology
   def maybe_delete_link(port)
     @links.each do |each|
       next unless each.connect_to?(port)
-      changed
       @links -= [each]
-      notify_observers :delete_link, each, self
+      port_a = Port.new(each.dpid_a, each.port_a)
+      port_b = Port.new(each.dpid_b, each.port_b)
+      maybe_callback :delete_link, port_a, port_b, self
+    end
+  end
+
+  def maybe_callback(method, *args)
+    @observers.each do |each|
+      next unless each.respond_to?(method)
+      each.__send__ method, *args
     end
   end
 end
