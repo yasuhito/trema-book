d5d869561f480096b89aef336112032f9fe543eb
Yasuhito Takamiya <yasuhito@gmail.com>
2015-04-14 16:05:06 +0900

    Add RSpec tests.

--- spec/lib/patch_panel_spec.rb
+++ spec/lib/patch_panel_spec.rb
@@ -15,7 +15,7 @@ describe PatchPanel do
     describe '#switch_ready(0xabc)' do
       Given { patch_panel.switch_ready(0xabc) }
 
-      describe 'create_patch' do
+      describe '#create_patch' do
         Given { allow(patch_panel).to receive(:send_flow_mod_add) }
 
         context 'with 0xabc, 1, 2' do
@@ -30,6 +30,23 @@ describe PatchPanel do
                                    with(0xabc,
                                         match: Pio::Match.new(in_port: 2),
                                         actions: Pio::SendOutPort.new(1)))
+          end
+        end
+      end
+
+      describe '#delete_patch' do
+        Given { allow(patch_panel).to receive(:send_flow_mod_delete) }
+
+        context 'with 0xabc, 1, 2' do
+          When { patch_panel.delete_patch 0xabc, 1, 2 }
+
+          Then do
+            expect(patch_panel).to(have_received(:send_flow_mod_delete).
+                                   with(0xabc,
+                                        match: Pio::Match.new(in_port: 1)))
+            expect(patch_panel).to(have_received(:send_flow_mod_delete).
+                                   with(0xabc,
+                                        match: Pio::Match.new(in_port: 2)))
           end
         end
       end
